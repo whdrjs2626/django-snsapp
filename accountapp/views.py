98581253fbb7ac826a622bc001227be0c66516ee
whdrjs2626 <75626100+whdrjs2626@users.noreply.github.com>
2022-02-06 01:00:05 +0900

    18강 - DB정보 접근(템플릿 내 for loop 사용)

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from accountapp.models import HelloWorld
 
@@ -7,11 +8,14 @@ from accountapp.models import HelloWorld
 def hello_world(request):
     if request.method == "POST":
         temp = request.POST.get('hello_world_input') # POST 요청의 폼에서 hello_world_input인 데이터를 가져와라
+
         new_hello_world = HelloWorld() # HelloWorld 객체(모델) 생성
         new_hello_world.text = temp # temp를 text로
         new_hello_world.save() # 저장
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world}) # 첫번쨰 인자는 request, 두번째는 템플릿 명
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
         # POST 요청에 의한 응답으로 text에 값을 보냄
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
