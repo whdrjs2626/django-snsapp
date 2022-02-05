97d9829d887649ec28698996b2054e1f12e953d3
whdrjs2626 <75626100+whdrjs2626@users.noreply.github.com>
2022-02-06 00:42:36 +0900

    16강 - GET, POST

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,4 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html') # 첫번쨰 인자는 request, 두번째는 템플릿 명
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!!!'}) # 첫번쨰 인자는 request, 두번째는 템플릿 명
+        # POST 요청에 의한 응답으로 text에 값을 보냄
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+
