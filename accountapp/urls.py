5f810b49f5c9d4f8157d232bf641970e9d9fd9fe
whdrjs2626 <75626100+whdrjs2626@users.noreply.github.com>
2022-02-06 02:18:48 +0900

    24강 - DetailView - 마이페이지 구현

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView  # 지정한 뷰 함수 가져오기
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView  # 지정한 뷰 함수 가져오기
 
 app_name = "accountapp"
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', AccountCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'), # detail은 특정 유저의 정보를 봐야 함 - 계정의 ID(pk)가 필요함, <int:pk> - pk라는 이름의 int 정보를 받겠다.
 ]
