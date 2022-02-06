908bed04b6fdf7d6345bb5a2ddca5b6f636f2fe4
whdrjs2626 <75626100+whdrjs2626@users.noreply.github.com>
2022-02-06 19:41:23 +0900

    28강 - decorator 추가, 인증 시스템 구현

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,27 +1,35 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView, DetailView, DeleteView
 
+from accountapp.decorator import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
+has_ownership = [account_ownership_required, login_required] # decorater 배열
 
+@login_required # 로그인 인증 decorator - 함수형 뷰 전용
 def hello_world(request):
     if request.method == "POST":
-        temp = request.POST.get('hello_world_input') # POST 요청의 폼에서 hello_world_input인 데이터를 가져와라
+        temp = request.POST.get('hello_world_input')  # POST 요청의 폼에서 hello_world_input인 데이터를 가져와라
 
-        new_hello_world = HelloWorld() # HelloWorld 객체(모델) 생성
-        new_hello_world.text = temp # temp를 text로
-        new_hello_world.save() # 저장
+        new_hello_world = HelloWorld()  # HelloWorld 객체(모델) 생성
+        new_hello_world.text = temp  # temp를 text로
+        new_hello_world.save()  # 저장
 
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
         # POST 요청에 의한 응답으로 text에 값을 보냄
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+
 
 class AccountCreateView(CreateView): # Create
     model = User # 장고에서 제공하는 User 객체
@@ -34,6 +42,8 @@ class AccountDetailView(DetailView): # Read - Detail
     context_object_name = 'target_user' # detail 페이지에서 user의 정보를 출력하면 무조건 나의 정보만 띄운다. 즉 다른 사람의 페이지로 들어가도 내정보가 나옴 / 따라서 이렇게 target_user를 설정
     template_name = 'accountapp/detail.html'
 
+@method_decorator(has_ownership, 'get') # 클래스형 뷰에 인증 decorator을 사용하기 위해서 method_decorator를 사용해야 함
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView): # Update - Create와 똑같다.
     model = User
     context_object_name = 'target_user'
@@ -41,6 +51,8 @@ class AccountUpdateView(UpdateView): # Update - Create와 똑같다.
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
