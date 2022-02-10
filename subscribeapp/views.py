e0981105c07ef245911baa89aacfe8d77d2abf57
whdrjs2626 <75626100+whdrjs2626@users.noreply.github.com>
2022-02-11 01:35:26 +0900

    43강 - 구독한 Project들의 Article List - Subscription List 구현(Field Lookup 사용)

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
 
@@ -25,3 +26,16 @@ class SubscriptionView(RedirectView):
             Subscription(user=user, project=project).save() # 구독을 안했다면 - 구독
 
         return super(SubscriptionView, self).get(request, *args, **kwargs)
+
+@method_decorator(login_required, 'get')
+class SubscriptionListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'subscribeapp/list.html'
+    paginate_by = 5
+
+    def get_queryset(self): # 구독정보 조건을 달아줌
+       projects = Subscription.objects.filter(user=self.request.user).values_list('project') # subscribe 모델이 가진 속성 중 project로 리스트를 만든다.
+       # 즉 요청을 한 user가 구독한 모든 project 정보를 projects로 만듦
+       article_list = Article.objects.filter(project__in=projects) # field lookup 기능 WHERE project IN - projects안에 있는 project 각각의 article
+       return article_list
